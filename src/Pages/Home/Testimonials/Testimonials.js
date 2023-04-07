77ac85194de6bcb1f521b4c795c0ea1822fb3ff4
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-07 17:08:36 +0600

    react slick install for testimonials

---
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
