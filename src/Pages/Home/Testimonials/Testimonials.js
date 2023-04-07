365ede3dcc3ad756b5bb82ed74dfae1d63c85634
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-07 17:26:38 +0600

    Testimonials object is created

--- src/Pages/Home/Testimonials/Testimonials.js
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -3,10 +3,58 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import testimonialImage1 from '../../../Resource/Images/Testimonials/img-18.png'
+import testimonialImage2 from '../../../Resource/Images/Testimonials/img-19.png'
 
 const Testimonials = () => {
+
+    const testimonialsInfo = [
+        {
+          id: 1,
+          title: "WEST MIDLANDS FIRE SERVICE",
+          link_name: "Watch Video",
+          link_url: "",
+          reviewer_name: "Adrian Scoyne",
+          reviewer_title: "INFRASTRUCTURE ENGINEER",
+          product_image: testimonialImage2
+        },
+        {
+          id: 2,
+          title: "WEST MIDLANDS FIRE SERVICE",
+          link_name: "Watch Video",
+          link_url: "",
+          reviewer_name: "Adrian Scoyne",
+          reviewer_title: "INFRASTRUCTURE ENGINEER",
+          product_image: testimonialImage1
+        },
+        {
+          id: 3,
+          title: "WEST MIDLANDS FIRE SERVICE",
+          link_name: "Watch Video",
+          link_url: "",
+          reviewer_name: "Adrian Scoyne",
+          reviewer_title: "INFRASTRUCTURE ENGINEER",
+          product_image: testimonialImage2
+        },
+        {
+          id: 4,
+          title: "WEST MIDLANDS FIRE SERVICE",
+          link_name: "Watch Video",
+          link_url: "",
+          reviewer_name: "Adrian Scoyne",
+          reviewer_title: "INFRASTRUCTURE ENGINEER",
+          product_image: testimonialImage1
+        },
+       
+      ];
+      
+
   return (
-    <div>Testimonials</div>
+    <div>
+
+
+
+    </div>
   )
 }
 
