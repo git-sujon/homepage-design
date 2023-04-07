eb4809ef15d047fdca237f79901b1a4641692abb
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-07 15:13:34 +0600

    footer Menus objects are created

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,8 +1,75 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const services = [
+    {
+      id: '01',  
+      name:  "Telephone Systems",
+      url: ""
+    },
+    {
+      id: '02',
+      name: "Lines & Calls",
+      url: ""
+    },
+    {
+      id: '03',
+      name: "Internet",
+      url: ""
+    },
+    {
+      id: '04',
+      name: "Mobile & 4G",
+      url: ""
+    },
+    {
+      id: '05',
+      name: "Cabling",
+      url: ""
+    },
+    {
+      id: '06',
+      name: "Wifi",
+      url: ""
+    },
+    {
+      id: '07',
+      name: "Hosted Telephony System",
+      url: ""
+    }
+  ];
+
+  const useful_links = [
+    {
+      id: '01',  
+      name: "Our Brochure",
+      url: ""
+    },
+    {
+      id: '02',
+      name: "Order Form",
+      url: ""
+    },
+    {
+      id: '03',
+      name: "Finance Calculator",
+      url: ""
+    },
+    {
+      id: '04',
+      name: "Blog",
+      url: ""
+    }
+  ];
+  
+  
+  
+
   return (
-    <div>Footer</div>
+    <div>
+ 
+  </div>
   )
 }
 
