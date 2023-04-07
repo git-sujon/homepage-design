f3a53742f8fa43100ad5b8379a0eca61c5bb60d8
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-07 23:17:52 +0600

    Subscribe section complete

--- src/Pages/Home/Subscribe/Subscribe.js
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Subscribe = () => {
-
-    
-
   return (
-    <div className='text-center'>
-        <h2>Let's Connect</h2>
-        <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
-    <div class="absolute inset-0 w-3 bg-gradient-to-r from-[#eba56c] to-[#67b1e4] border-[#eba56c]  transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-    <span class="relative text-[#67b1e4] group-hover:text-white border-[#eba56c] font-bold">GET IN TOUCH</span>
-  </button>
+    <div className="text-center pb-10">
+      <h2 className="text-4xl font-bold  text-[#29303B]">Let's Connect</h2>
+      <button class="my-10 group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+        <div class="absolute inset-0 w-3 bg-gradient-to-r from-[#eba56c] to-[#67b1e4] border-[#eba56c]  transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+        <span class="relative text-[#67b1e4] group-hover:text-white border-[#eba56c] font-bold">
+          GET IN TOUCH
+        </span>
+      </button>
+      <h2 className="text-4xl font-semibold text-[#29303B]">Stay updated.</h2>
+      <p className="text-lg font-semibold text-[#898e98]">
+        Discover the latest new
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Subscribe
+export default Subscribe;
